3278bbe47944b712fdf7a1566479faea6da5960d
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-17 10:48:07 +0900

    Implement

--- packages/css-loader/lib/index.ts
+++ packages/css-loader/lib/index.ts
@@ -1,1 +1,66 @@
-import { transform } from "@swc/css";
+import {
+    CssModuleTransformOptions,
+    TransformOptions,
+    transform,
+    transformSync,
+} from "@swc/css";
+import type * as webpack from "webpack";
+
+export interface LoaderOptions {
+    sync?: boolean;
+    parseMap?: boolean;
+    sourceMap?: boolean;
+    cssModules?: CssModuleTransformOptions;
+}
+
+export default function loader(
+    this: webpack.LoaderContext<LoaderOptions>,
+    source: string,
+    inputSourceMap: any
+) {
+    // Make the loader async
+    const callback = this.async();
+    const filename = this.resourcePath;
+
+    const loaderOptions = this.getOptions();
+
+    if (inputSourceMap && typeof inputSourceMap === "object") {
+        inputSourceMap = JSON.stringify(inputSourceMap);
+    }
+
+    const sync = loaderOptions.sync;
+    const parseMap = loaderOptions.parseMap;
+
+    let transformOptions: TransformOptions = {
+        sourceMap: loaderOptions.sourceMap,
+        filename,
+        cssModules: loaderOptions.cssModules,
+        minify: false,
+    };
+
+    try {
+        if (sync) {
+            const output = transformSync(source, transformOptions);
+            callback(
+                null,
+                output.code,
+                parseMap ? JSON.parse(output.map!) : output.map
+            );
+        } else {
+            transform(source, transformOptions).then(
+                (output) => {
+                    callback(
+                        null,
+                        output.code,
+                        parseMap ? JSON.parse(output.map!) : output.map
+                    );
+                },
+                (err) => {
+                    callback(err);
+                }
+            );
+        }
+    } catch (e: any) {
+        callback(e);
+    }
+}
