6ba60e207b9360d42a23647ba8a275c482ea4184
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-21 08:03:26 +0900

    import

--- packages/css-loader/examples/modules/index.js
+++ packages/css-loader/examples/modules/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 import classes from "./App.module.css";
 
 function App() {
-    return <h1 className={classes.text}>JSX is working!</h1>;
+    return (
+        <div>
+            <h1 className={classes.text}>JSX is working!</h1>
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
