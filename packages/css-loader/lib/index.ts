1f35b07d793732e3a2efb6d3b27a20ffd010011f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-21 10:17:54 +0900

    `stringifyRequest`

--- packages/css-loader/lib/index.ts
+++ packages/css-loader/lib/index.ts
@@ -15,6 +15,7 @@ import {
     ApiParam,
     CssTransformResult,
 } from "./codegen.js";
+import path from "path";
 
 export interface Options {
     sourceMap: boolean;
@@ -97,7 +98,7 @@ export default async function loader(
         // TODO
         modules: {
             namedExport: false,
-            exportOnlyLocals: false,
+            exportOnlyLocals: true,
             exportLocalsConvention: (name: string) => name,
         },
         sourceMap: false,
@@ -109,7 +110,33 @@ export default async function loader(
         exportLocalsConventionType: "asIs",
     };
 
-    const transformResult = await transform(source, transformOptions);
+    if (options.modules.exportOnlyLocals !== true) {
+        imports.unshift({
+            type: "api_import",
+            importName: "___CSS_LOADER_API_IMPORT___",
+            url: stringifyRequest(this, require.resolve("./runtime/api")),
+        });
+
+        if (options.sourceMap) {
+            imports.unshift({
+                importName: "___CSS_LOADER_API_SOURCEMAP_IMPORT___",
+                url: stringifyRequest(
+                    this,
+                    require.resolve("./runtime/sourceMaps")
+                ),
+            });
+        } else {
+            imports.unshift({
+                importName: "___CSS_LOADER_API_NO_SOURCEMAP_IMPORT___",
+                url: stringifyRequest(
+                    this,
+                    require.resolve("./runtime/noSourceMaps")
+                ),
+            });
+        }
+    }
+
+    const transformResult = await transformSync(source, transformOptions);
     console.log(`Deps string: ${transformResult.deps!}`);
     const deps = JSON.parse(transformResult.deps!);
     const result: CssTransformResult = {
@@ -142,3 +169,63 @@ export default async function loader(
 }
 
 export const raw = true;
+
+const matchRelativePath = /^\.\.?[/\\]/;
+
+function isAbsolutePath(str: string) {
+    return path.posix.isAbsolute(str) || path.win32.isAbsolute(str);
+}
+
+function isRelativePath(str: string) {
+    return matchRelativePath.test(str);
+}
+
+// TODO simplify for the next major release
+function stringifyRequest(
+    loaderContext: webpack.LoaderContext<LoaderOptions>,
+    request: string
+) {
+    if (
+        typeof loaderContext.utils !== "undefined" &&
+        typeof loaderContext.utils.contextify === "function"
+    ) {
+        return JSON.stringify(
+            loaderContext.utils.contextify(
+                loaderContext.context || loaderContext.rootContext,
+                request
+            )
+        );
+    }
+
+    const splitted = request.split("!");
+    const { context } = loaderContext;
+
+    return JSON.stringify(
+        splitted
+            .map((part) => {
+                // First, separate singlePath from query, because the query might contain paths again
+                const splittedPart = part.match(/^(.*?)(\?.*)/);
+                const query = splittedPart ? splittedPart[2] : "";
+                let singlePath = splittedPart ? splittedPart[1] : part;
+
+                if (isAbsolutePath(singlePath) && context) {
+                    singlePath = path.relative(context, singlePath);
+
+                    if (isAbsolutePath(singlePath)) {
+                        // If singlePath still matches an absolute path, singlePath was on a different drive than context.
+                        // In this case, we leave the path platform-specific without replacing any separators.
+                        // @see https://github.com/webpack/loader-utils/pull/14
+                        return singlePath + query;
+                    }
+
+                    if (isRelativePath(singlePath) === false) {
+                        // Ensure that the relative path starts at least with ./ otherwise it would be a request into the modules directory (like node_modules).
+                        singlePath = `./${singlePath}`;
+                    }
+                }
+
+                return singlePath.replace(/\\/g, "/") + query;
+            })
+            .join("!")
+    );
+}
