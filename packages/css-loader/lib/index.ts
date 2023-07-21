6b9f242fb90e93b16af5e0d6e6f2f5ae801e45ec
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-21 10:21:13 +0900

    exportOnlyLocals

--- packages/css-loader/lib/index.ts
+++ packages/css-loader/lib/index.ts
@@ -98,7 +98,7 @@ export default async function loader(
         // TODO
         modules: {
             namedExport: false,
-            exportOnlyLocals: true,
+            exportOnlyLocals: false,
             exportLocalsConvention: (name: string) => name,
         },
         sourceMap: false,
