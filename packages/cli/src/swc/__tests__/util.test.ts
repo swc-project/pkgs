a7adab9e15d1212b5c2a987bc9dfebed74056c86
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-01 19:54:26 +0900

    fix windows

--- packages/cli/src/swc/__tests__/util.test.ts
+++ packages/cli/src/swc/__tests__/util.test.ts
@@ -5,18 +5,18 @@ describe("getDest", () => {
     it("does not modify the filename by default", () => {
         expect(
             getDest(join(process.cwd(), "src/path/name.ts"), "foo/bar", false)
-        ).toEqual("foo/bar/src/path/name.ts");
+        ).toEqual(join("foo", "bar", "src", "path", "name.ts"));
     });
 
     it("when stripLeadingPaths is true, it removes leading paths", () => {
         expect(
             getDest(join(process.cwd(), "src/path/name.ts"), "foo/bar", true)
-        ).toEqual("foo/bar/path/name.ts");
+        ).toEqual(join("foo", "bar", "path", "name.ts"));
     });
 
     it("when stripLeadingPaths is true, it also resolves relative paths", () => {
         expect(
             getDest(join(process.cwd(), "../../path/name.ts"), "foo/bar", true)
-        ).toEqual("foo/bar/path/name.ts");
+        ).toEqual(join("foo", "bar", "path", "name.ts"));
     });
 });
