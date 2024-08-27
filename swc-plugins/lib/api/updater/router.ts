e6050bd4d3183f2eef464bf31f4b084ab19b72d4
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-08-28 06:56:16 +0900

    feat: Add endpoint for updating runtimes (#57)

--- swc-plugins/lib/api/updater/router.ts
+++ swc-plugins/lib/api/updater/router.ts
@@ -1,6 +1,7 @@
 import { publicProcedure, router } from "@/lib/base";
 import { db } from "@/lib/prisma";
 import { TRPCError } from "@trpc/server";
+import semver from "semver";
 import { z } from "zod";
 
 function validateToken(token: string) {
@@ -14,19 +15,24 @@ function validateToken(token: string) {
   });
 }
 
-const NpmPackageVersionSchema = z.object({
+const PackageVersionSchema = z.object({
   version: z.string(),
   swcCoreVersion: z.string(),
 });
 
-const NpmPackageSchema = z.object({
+const PackageSchema = z.object({
   name: z.string(),
-  versions: z.array(NpmPackageVersionSchema),
+  versions: z.array(PackageVersionSchema),
 });
 
 export const UpdateWasmPluginsInputSchema = z.object({
   token: z.string(),
-  pkgs: z.array(NpmPackageSchema),
+  pkgs: z.array(PackageSchema),
+});
+
+export const UpdateRuntimesInputSchema = z.object({
+  token: z.string(),
+  pkgs: z.array(PackageSchema),
 });
 
 export const updaterRouter = router({
@@ -71,6 +77,76 @@ export const updaterRouter = router({
             },
             create: {
               pluginId: plugin.id,
+              version: version.version,
+              compatRangeId: compatRange.id,
+              swcCoreVersion,
+            },
+            update: {
+              compatRangeId: compatRange.id,
+              swcCoreVersion,
+            },
+          });
+        }
+      }
+    }),
+
+  updateRuntimes: publicProcedure
+    .input(UpdateRuntimesInputSchema)
+    .output(z.void())
+    .mutation(async ({ input, ctx }) => {
+      validateToken(input.token);
+
+      const compatRanges = await db.compatRange.findMany({
+        select: {
+          id: true,
+          from: true,
+          to: true,
+        },
+      });
+
+      // Runtimes has so many versions so we need a faster logic.
+      function byVersion(swcCoreVersion: string) {
+        for (const range of compatRanges) {
+          if (
+            semver.gte(swcCoreVersion, range.from) &&
+            (range.to === "*" || semver.lte(swcCoreVersion, range.to))
+          ) {
+            return range;
+          }
+        }
+      }
+
+      for (const pkg of input.pkgs) {
+        const runtime = await db.swcRuntime.upsert({
+          where: {
+            name: pkg.name,
+          },
+          create: {
+            name: pkg.name,
+          },
+          update: {},
+        });
+
+        for (const version of pkg.versions) {
+          const swcCoreVersion = version.swcCoreVersion;
+          const compatRange = byVersion(swcCoreVersion);
+
+          if (!compatRange) {
+            throw new TRPCError({
+              code: "NOT_FOUND",
+              message: `Compat range not found for SWC core version ${swcCoreVersion}`,
+            });
+          }
+
+          await db.swcRuntimeVersion.upsert({
+            where: {
+              runtimeId_version: {
+                runtimeId: runtime.id,
+                version: version.version,
+              },
+            },
+            create: {
+              runtimeId: runtime.id,
               version: version.version,
               compatRangeId: compatRange.id,
               swcCoreVersion,
