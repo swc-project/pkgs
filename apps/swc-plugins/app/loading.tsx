a3889a338813ae17388e848aa15d819bad8e681a
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-10-23 12:25:29 +0900

    chore(ci): Fix CI (#68)

--- apps/swc-plugins/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="flex min-h-screen w-full items-center justify-center">
-            <div className="animate-pulse">Loading...</div>
-        </div>
-    );
-}
