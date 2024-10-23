9fae37f0180a0c8f19264dfe857642189b88bbc0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-23 12:21:37 +0900

    Remove plugins

--- apps/swc-plugins/lib/trpc/web-client.ts
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import { ApiRouter } from "@/lib/api/router";
-import { createTRPCReact } from "@trpc/react-query";
-
-export const apiClient = createTRPCReact<ApiRouter>({});
--- apps/swc-plugins/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        typedRoutes: true,
-    },
-};
-
-export default nextConfig;
